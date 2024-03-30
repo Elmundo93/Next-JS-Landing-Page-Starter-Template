e911d18c1645df1ff8f2baca59abf3f2f6c719b4
Lemont <l.mrutschock@gmx.de>
2024-03-30 03:25:14 +0100

    changes

--- src/templates/Arrow.tsx
+++ src/templates/Arrow.tsx
@@ -1,12 +1,27 @@
-import Lottie from 'lottie-react';
+'use client';
+
+import dynamic from 'next/dynamic';
 import React from 'react';
 
 import animationData from '../utils/ArrowAnimation.json';
 
-const MovingArrow = () => (
-  <div className="arrow-animation" id="arrow-animation">
-    <Lottie animationData={animationData} className="ArrowAnimation" />
-  </div>
-);
+// Import the Lottie component
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
+
+const MovingArrow = () => {
+  return (
+    <div
+      className="arrow arrow:hover flex rotate-180 items-center justify-center transition-transform"
+      id="arrow-animation"
+    >
+      <React.Suspense fallback={<div>Loading animation...</div>}>
+        <Lottie
+          animationData={animationData}
+          className="ArrowAnimation size-20"
+        />
+      </React.Suspense>
+    </div>
+  );
+};
 
 export default MovingArrow;
--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,26 +3,27 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
+    title="Unser Ziel"
+    description="Wir vom 'Wir helfen aus e.V.' glauben daran, dass wir mit unserem Netzwerk die Helferkultur auf regionaler Ebene fördern. Viele Aufgaben lassen sich mit mehr Händen schneller bewältigen & deshalb verbinden wir Menschen mit unserer App!"
   >
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature.svg"
+      title="Hilfe für Eigentümer"
+      description="Im Alltag fallen regelmäßig Arbeiten an, das kennen wir alle. & vor allem als Eigentümer können diese Alltagspflichten, je nach Art, mehr Arbeitskraft fordern als im Haus vertreten ist. Wenn dann auch noch der Nachbar keine Zeit hat, steht man vor einer eigentlich vergnügsamen Aufgabe, welche zum stressigen Wochen-, oder vielleicht sogar Monatsprojekt wird."
+      image="/assets/images/img_eigentümer.jpg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature2.svg"
+      title="Hilfe für beeinträchtigte Menschen oder Senioren"
+      description="Kleinere Aufgaben wie Gassie gehen, Einkaufen oder sich um den Garten kümmern, werden für Menschen mit einer Beeinträchtigung oft schon zu einem Problem. Hier reichen oft schon kleine Hilfestellungen um einen imensen Unterschied in der Lebensqualität zu erzielen."
+      image="/assets/images/img_senioren.jpg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
+      title="Hilfe beim Prozess"
+      description="Ist dann eine helfende Hand gefunden, kommen fragen auf wie:'Falls denn was passiert, ist man denn dann Versichert?'
+      Bei Fragen jeder Art stehen wir als Verein ihnen zur Seite. Des weiteren finden Sie alle nötigen Links zur Minijob-Zentrale bei uns auf der Homepage."
+      image="/assets/images/process_img.jpg"
       imageAlt="Third feature alt text"
     />
   </Section>
